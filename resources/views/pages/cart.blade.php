72c69fb2a3e960740a6d95ce01ce7d6fef7ce9b7
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2022-06-14 14:42:26 +0700

    New cart pages

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <main class="main">
-    <div class="page-header text-center" style="background-image: url('/user/assets/images/page-header-bg.jpg')">
+    <div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
         <div class="container">
             <h1 class="page-title">Shopping Cart<span>Shop</span></h1>
         </div><!-- End .container -->
@@ -25,15 +25,15 @@
         <div class="cart">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-9">
+                    <div class="col-lg-12">
                         <table class="table table-cart table-mobile">
                             <thead>
                                 <tr>
-                                    <th>Product</th>
-                                    <th>Price</th>
-                                    <th>Quantity</th>
-                                    <th>Total</th>
-                                    <th></th>
+                                    <th class="column-cart">Product</th>
+                                    <th class="column-cart">Price</th>
+                                    <th class="column-cart">Quantity</th>
+                                    <th class="column-cart">Total</th>
+                                    <th class="column-cart"></th>
                                 </tr>
                             </thead>
 
@@ -41,7 +41,7 @@
                                 @php $total = 0; $totalItem = 0; @endphp
                                 @foreach ($cartItems as $data)
                                     <tr class="product_data">
-                                        <td class="product-col ">
+                                        <td class="product-col">
                                             <div class="product">
                                                 <figure class="product-media">
                                                     <a href="#">
@@ -54,10 +54,10 @@
                                                 </h3><!-- End .product-title -->
                                             </div><!-- End .product -->
                                         </td>
-                                        <td class="price-col">IDR. <br>{{ number_format($data->products->price, 2, ',', '.') }}</td>
+                                        <td class="price-col">IDR. {{ number_format($data->products->price, 2, ',', '.') }}</td>
                                         <td class="quantity-col">
                                             <input type="hidden" class="prod_id" value="{{ $data->products_id }}">
-
+                                            
                                             @if ($data->products->qty >= $data->products_qty)
                                                 <div class="cart-product-quantity">
                                                     <input type="number" class="form-control" value="{{ $data->products_qty }}" min="1" max="10" step="1" data-decimals="0" required>
@@ -72,82 +72,25 @@
                                                 
                                                 @php $totalItem = $data->products->price * 0; @endphp
                                             @endif
-                                            
                                         </td>
-                                        
                                         <td class="total-col">IDR. {{ number_format($totalItem, 2, ',', '.') }}</td>
-                                        <td class="remove-col">
-                                            <button class="btn-remove delete-cart-item">
+                                        <td class="remove-col"><button class="btn-remove">
                                             <i class="icon-close"></i>
                                         </button></td>
                                     </tr>
-
                                 @endforeach
                             </tbody>
                         </table><!-- End .table table-wishlist -->
+
+                        <div class="cart-bottom">
+                            <div class="cart-subtotal">
+                                <button class="btn btn-outline-dark-2-cart">SUBTOTAL : &nbsp; <span class="idr-subtotal">IDR. {{ number_format($total, 2, ',', '.') }}</span></button>
+                            </div><!-- End .cart-discount -->
+
+                            <a href="{{ route('product.index') }}" class="btn btn-outline-dark-2"><span>CONTINUE SHOPPING</span><i class="icon-refresh"></i></a>
+                            <a href="{{ url('checkout') }}" class="btn btn-outline-primary-2"><span>PROCEED TO ORDER</span></a>
+                        </div><!-- End .cart-bottom -->
                     </div><!-- End .col-lg-9 -->
-                    <aside class="col-lg-3">
-                        <div class="summary summary-cart">
-                            <h3 class="summary-title">Cart Total</h3><!-- End .summary-title -->
-
-                            <table class="table table-summary">
-                                <tbody>
-                                    <tr class="summary-subtotal">
-                                        <td>Subtotal:</td>
-                                        <td>IDR. <br>{{ number_format($total, 2, ',', '.') }}</td>
-                                    </tr><!-- End .summary-subtotal -->
-                                    <tr class="summary-shipping">
-                                        <td>Shipping:</td>
-                                        <td>&nbsp;</td>
-                                    </tr>
-
-                                    <tr class="summary-shipping-row">
-                                        <td>
-                                            <div class="custom-control custom-radio">
-                                                <input type="radio" id="free-shipping" name="shipping" class="custom-control-input">
-                                                <label class="custom-control-label" for="free-shipping">Free Shipping</label>
-                                            </div><!-- End .custom-control -->
-                                        </td>
-                                        <td>$0.00</td>
-                                    </tr><!-- End .summary-shipping-row -->
-
-                                    <tr class="summary-shipping-row">
-                                        <td>
-                                            <div class="custom-control custom-radio">
-                                                <input type="radio" id="standart-shipping" name="shipping" class="custom-control-input">
-                                                <label class="custom-control-label" for="standart-shipping">Standart:</label>
-                                            </div><!-- End .custom-control -->
-                                        </td>
-                                        <td>$10.00</td>
-                                    </tr><!-- End .summary-shipping-row -->
-
-                                    <tr class="summary-shipping-row">
-                                        <td>
-                                            <div class="custom-control custom-radio">
-                                                <input type="radio" id="express-shipping" name="shipping" class="custom-control-input">
-                                                <label class="custom-control-label" for="express-shipping">Express:</label>
-                                            </div><!-- End .custom-control -->
-                                        </td>
-                                        <td>$20.00</td>
-                                    </tr><!-- End .summary-shipping-row -->
-
-                                    <tr class="summary-shipping-estimate">
-                                        <td>Estimate for Your Country<br> <a href="dashboard.html">Change address</a></td>
-                                        <td>&nbsp;</td>
-                                    </tr><!-- End .summary-shipping-estimate -->
-
-                                    <tr class="summary-total">
-                                        <td>Total:</td>
-                                        <td>$160.00</td>
-                                    </tr><!-- End .summary-total -->
-                                </tbody>
-                            </table><!-- End .table table-summary -->
-
-                            <a href="{{ url('checkout') }}" class="btn btn-outline-primary-2 btn-order btn-block">PROCEED TO CHECKOUT</a>
-                        </div><!-- End .summary -->
-
-                        <a href="category.html" class="btn btn-outline-dark-2 btn-block mb-3"><span>CONTINUE SHOPPING</span><i class="icon-refresh"></i></a>
-                    </aside><!-- End .col-lg-3 -->
                 </div><!-- End .row -->
             </div><!-- End .container -->
         </div><!-- End .cart -->
